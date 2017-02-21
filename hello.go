bafa9dde85724c2c8e05c2f7e653debb28fe75ef
John Yang <byjohnyang@gmail.com>
2017-02-20 19:27:31 -0800

    Initial Commit

---
+++ hello.go
@@ -0,0 +1,19 @@
+import (
+	"fmt"
+	"time"
+	"os"
+    
+	bw2 "gopkg.in/immesys/bw2bind.v5"
+)
+
+func main() {
+	bwClient, err := bw2.Connect("")
+	if err != nil {
+		fmt.Printf("Failed to connect to Bosswave agent: %v\n", err)
+		os.Exit(1)
+	}
+	for {
+		fmt.Println("Hello World")
+		time.Sleep(10*time.Second)
+	}
+}
