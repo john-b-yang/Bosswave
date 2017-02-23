ebdd1bb197606d0d71cd28483646542c67e7911a
John Yang <byjohnyang@gmail.com>
2017-02-22 18:50:51 -0800

    Publish done, subscribe is next hw!

---
+++ Subscribe/goodbye.go
@@ -0,0 +1,44 @@
+// Reference:
+// 1. Jack's Demo Repo: https://github.com/jhkolb/demosvc/blob/master/main.go
+// 2. Documentation: https://godoc.org/gopkg.in/immesys/bw2bind.v3#BW2Client.SetEntity
+
+// Steps:
+// 1. check permission with setentity, set entity from environment variables, setentityfromenviron
+// 2. Create string payload object (createstringpayloadobject?)
+// 3. Replace fmt.println with bosswave publish (publish to namespace)
+package main
+import (
+	"fmt"
+	"time"
+	"os"
+
+	bw2 "gopkg.in/immesys/bw2bind.v5"
+)
+
+func main() {
+	message := "Message"
+	bwClient, err := bw2.Connect("")
+	bwClient.SetEntityFromEnvironOrExit()
+
+	if err != nil {
+		fmt.Printf("Failed to connect to Bosswave agent: %v\n", err)
+		os.Exit(1)
+	}
+
+	for {
+		payload := bw2.CreateStringPayloadObject(message)
+		err = bwClient.Publish(&bw2.PublishParams{
+
+			URI:            "john/test",
+			AutoChain:      true,
+
+
+			PayloadObjects: []bw2.PayloadObject{payload},
+		})
+		if (err != nil) {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		time.Sleep(10*time.Second)
+	}
+}
